4a57c32a6089a49af377dda42949fb334aa3947c
thiagomay1 <thiago.may1@gmail.com>
2020-10-14 21:19:10 -0300

    Adicionando validações nos campos de Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   has_many :payment
   belongs_to :shipping
   belongs_to :buyer
+  validates :external_id, :external_store_id, :date_created, :total_amount, :total_shipping, :paid_amount, :payment, :order_item, :buyer, :shipping, presence: true
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,79 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "order should be valid" do
+     order = build_order
+     assert order.valid?
+   end
+
+   test "id should not be empty" do
+     order = build_order
+     order.external_id = nil
+     assert order.invalid?
+     assert_equal "Id can't be blank", order.errors.full_messages.first
+   end
+
+   test "store_id should not be empty" do
+     order = build_order
+     order.external_store_id = nil
+     assert order.invalid?
+     assert_equal "Store Id can't be blank", order.errors.full_messages.first
+   end
+
+   test "date_created should not be empty" do
+     order = build_order
+     order.date_created = nil
+     assert order.invalid?
+   end
+
+   test "total_amount should not be empty" do
+     order = build_order
+     order.total_amount = nil
+     assert order.invalid?
+   end
+
+   test "total_shipping should not be empty" do
+     order = build_order
+     order.total_shipping = nil
+     assert order.invalid?
+   end
+
+   test "paid_amount should not be empty" do
+     order = build_order
+     order.paid_amount = nil
+     assert order.invalid?
+   end
+
+   test "order_item should not be empty" do
+     order = build_order
+     order.order_item = []
+     assert order.invalid?
+   end
+
+   test "payments should not be empty" do
+     order = build_order
+     order.payment = []
+     assert order.invalid?
+   end
+
+   test "shipping should not be empty" do
+     order = build_order
+     order.shipping = nil
+     assert order.invalid?
+   end
+
+   test "buyer should not be empty" do
+     order = build_order
+     order.buyer = nil
+     assert order.invalid?
+   end
+
+   private
+   def build_order
+     buyer = Buyer.new
+     shipping = Shipping.new
+     payment = Payment.new
+     order_item = OrderItem.new(external_item_id: 1251)
+     Order.new(external_id: 123412, external_store_id: 1243, date_created: Time.now, total_amount: 49.9, total_shipping: 5.14, paid_amount: 55.04, payment: [payment], order_item: [order_item], buyer: buyer, shipping: shipping)
+   end
 end
