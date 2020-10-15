445913c6eb49c33d0b1482984ca9c70b94e4d6d2
thiagomay1 <thiago.may1@gmail.com>
2020-10-14 22:47:03 -0300

    Renderizando erros no controller

--- app/controllers/api/V1/order_controller.rb
+++ app/controllers/api/V1/order_controller.rb
@@ -5,6 +5,9 @@ module Api
       def create
         order_mapper = OrderMapper.new
         order = order_mapper.map_order(params)
+        if order.invalid?
+          render json: order.errors.full_messages
+        end
       end
     end
   end
