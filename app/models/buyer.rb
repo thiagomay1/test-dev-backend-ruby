37266a462e431ff6cd309a1c6e749776c9e6655a
thiagomay1 <thiago.may1@gmail.com>
2020-10-17 15:06:55 -0300

    Adicionado validações no modelo Buyer

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,16 @@
 class Buyer < ApplicationRecord
+  validates :external_id, :first_name, :last_name, :phone, presence: true
+  validate :phone_area_code_cannot_be_empty, :phone_number_cannot_be_empty
+
+  def phone_number_cannot_be_empty
+    if phone.present? && !phone['number'].present?
+       errors.add(:phone, "Number can't be blank")
+    end
+  end
+
+  def phone_area_code_cannot_be_empty
+    if phone.present? && !phone['area_code'].present?
+       errors.add(:phone, "Area Code can't be blank")
+    end
+  end
 end
